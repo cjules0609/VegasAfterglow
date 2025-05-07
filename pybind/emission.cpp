99aeff790ab1e2a0671ed3a28f5e55a110e4e04a
Connery Chen <connery@Connerys-MacBook-Pro.local>
2025-05-07 01:06:19 -0700

    refined emission class to generate lc and spec

--- pybind/emission.cpp
+++ pybind/emission.cpp
@@ -1,24 +1,9 @@
 #include "emission.h"
 
-// Light curve
-Emission::Emission(Real nu, const std::vector<Real>& t)
-    : nu(xt::adapt(std::vector<Real>{nu}) * unit::Hz),
-      t(xt::adapt(t) * unit::sec),
-      F_nu(xt::adapt(std::vector<Real>(t.size(), 0.0))) {}
+Emission::Emission(const Params& param, const ConfigParams& config) {
+    this->param = param;
+    this->config = config;
 
-// Spectrum
-Emission::Emission(const std::vector<Real>& nu, Real t)
-    : t(xt::adapt(std::vector<Real>{t}) * unit::sec),
-      nu(xt::adapt(nu) * unit::Hz),
-      F_nu(xt::adapt(std::vector<Real>(nu.size(), 0.0))) {}
-      
-
-void Emission::generate(const Params& param, const ConfigParams& config) {
-    Observer obs;
-    SynElectronGrid electrons;
-    SynPhotonGrid photons;
-
-    Medium medium;
     if (config.medium == "ism") {
         std::tie(medium.rho, medium.mass) = evn::ISM(param.n_ism / unit::cm3);
     } else if (config.medium == "wind") {
@@ -28,7 +13,6 @@ void Emission::generate(const Params& param, const ConfigParams& config) {
         return;
     }
 
-    Ejecta jet;
     Real E_iso = param.E_iso * unit::erg;
     Real Gamma0 = param.Gamma0;
 
@@ -45,19 +29,27 @@ void Emission::generate(const Params& param, const ConfigParams& config) {
         std::cerr << "Unknown jet structure: " << config.jet << std::endl;
         return;
     }
+}
 
+void Emission::observe(const Params& param, const ConfigParams& config, const Array& t) {
     auto coord = auto_grid(jet, t, param.theta_w, param.theta_v, config.z, config.phi_grid, config.theta_grid, config.t_grid);
-
     auto shock = generate_fwd_shock(coord, medium, jet, param.eps_e, param.eps_B, config.rtol);
-
+    
     obs.observe(coord, shock, config.lumi_dist * unit::cm, config.z);
 
     electrons = generate_syn_electrons(shock, param.p, param.xi);
     photons = generate_syn_photons(shock, electrons);
+}
 
-    if (t.size() > 1 && nu.size() == 1) {
-        F_nu = xt::view(obs.specific_flux(t, nu, photons), 0, xt::all()) / unit::Jy;
-    } else {
-        F_nu = xt::view(obs.spectra(nu, t, photons), 0, xt::all()) / unit::Jy;
-    }
+std::vector<Real> Emission::generate_lc(Real nu, const std::vector<Real>& t) {
+    observe(param, config, xt::adapt(t) * unit::sec);  // Perform observation step
+    auto Fv_model_xt = obs.specific_flux(xt::adapt(t) * unit::sec, xt::adapt(std::vector<Real>{nu}) * unit::Hz, photons) / unit::Jy;
+    return std::vector<Real>(Fv_model_xt.begin(), Fv_model_xt.end());
+}
+
+std::vector<Real> Emission::generate_spec(const std::vector<Real>& nu, Real t) {
+    observe(param, config, xt::adapt(std::vector<Real>{t}) * unit::sec);  // Perform observation step
+    auto Fv_model_xt = obs.spectra(xt::adapt(nu) * unit::Hz, xt::adapt(std::vector<Real>{t}) * unit::sec, photons) / unit::Jy;
+    
+    return std::vector<Real>(Fv_model_xt.begin(), Fv_model_xt.end());
 }
